21c1082dd90660a1ee4bae5f3bcbf5e84d1a9a99
dp_chua <dp_chua@hotmail.com>
2017-03-14 15:11:59 -0400

    created model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniquenes:true
+  has_many :workouts, dependent: :destroy
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,5 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :weights, dependent: :destroy
+  has_many :cardios, dependent: :destroy
 end
