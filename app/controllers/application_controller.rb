6b5b25f8572168620f18cc4ae41999f58068186d
dp_chua <dp_chua@hotmail.com>
2017-03-15 14:54:13 -0400

    auth stuff for backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::API
+
+  ##Code form GIPHY LAB
+
+  def authenticate
+      render json: {status: 401, message: "Unauthorized"} unless decode_token(bearer_token)
+  end
+
+  def authorize
+    render json: {status: 401, message: "Unauthorized"} unless current_user.id == params[:id].to_i
+  end
+
+  def bearer_token
+    pattern = /^Bearer /
+    header = request.env["HTTP_AUTHORIZATION"]
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
+
+  def current_user
+    return if !bearer_token
+
+    decoded_jwt = decode_token(bearer_token)
+
+    @current_user ||= User.find(decoded_jwt["user"]["id"])
+  end
+
+  def decode_token(token)
+    @token ||= JWT.decode(token, nil, false)[0]
+  rescue
+    render json: {status: 401, message: 'invalid or expired token'}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniquenes:true
+  validates :email, presence: true, uniquenes:true, format: /@/
+  # format: /@/ require @ in email. 
   has_many :workouts, dependent: :destroy
 end
