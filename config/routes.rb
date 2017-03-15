279fcce49347182319b139617f0d30051c684a25
dp_chua <dp_chua@hotmail.com>
2017-03-15 15:48:12 -0400

    raisl routes added in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,38 @@ Rails.application.routes.draw do
   resources :workouts do
     resources :weights
     resources :cardios
-  end 
-
-
+  end
 end
+
+
+# Prefix Verb   URI Pattern                                         Controller#Action
+# login_users
+#        POST   /users/login(.:format)                                    users#login
+#  users
+#        POST   /users(.:format)                                          users#create
+#   user
+#        GET    /users/:id(.:format)                                      users#show
+# workout_weights
+#        GET    /workouts/:workout_id/weights(.:format)                   weights#index
+#        POST   /workouts/:workout_id/weights(.:format)                   weights#create
+# workout_weight
+#        GET    /workouts/:workout_id/weights/:id(.:format)               weights#show
+#        PATCH  /workouts/:workout_id/weights/:id(.:format)               weights#update
+#        PUT    /workouts/:workout_id/weights/:id(.:format)               weights#update
+#        DELETE /workouts/:workout_id/weights/:id(.:format)               weights#destroy
+# workout_cardios
+#        GET    /workouts/:workout_id/cardios(.:format)                   cardios#index
+#        POST   /workouts/:workout_id/cardios(.:format)                   cardios#create
+# workout_cardio
+#        GET    /workouts/:workout_id/cardios/:id(.:format)               cardios#show
+#        PATCH  /workouts/:workout_id/cardios/:id(.:format)               cardios#update
+#        PUT    /workouts/:workout_id/cardios/:id(.:format)               cardios#update
+#        DELETE /workouts/:workout_id/cardios/:id(.:format)               cardios#destroy
+# workouts
+#        GET    /workouts(.:format)                                       workouts#index
+#        POST   /workouts(.:format)                                       workouts#create
+# workout
+#        GET    /workouts/:id(.:format)                                   workouts#show
+#        PATCH  /workouts/:id(.:format)                                   workouts#update
+#        PUT    /workouts/:id(.:format)                                   workouts#update
+#        DELETE /workouts/:id(.:format)                                   workouts#destroy
