0341385faf63c16c405a456fd86f8cae2ea33eb4
dp_chua <dp_chua@hotmail.com>
2017-03-15 15:35:15 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:creaet, :show, :destroy] do
+    resources :workouts do
+      resources :weights
+      resources :cardios
+
+    collection do
+      post '/login', to 'users#login'
+    end
+
+
+  end
+
+  
 end
