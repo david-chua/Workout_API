8b65afd68aaae6cf33c6c135da02a8fedbe265de
dp_chua <dp_chua@hotmail.com>
2017-03-16 17:54:40 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,45 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :show] do
+    resources :workouts do
+      resources :weights
+      resources :cardios
+    end
     collection do
       post '/login', to: 'users#login'
     end
-  end
-
-  resources :workouts do
-    resources :weights
-    resources :cardios
-  end
+end
 end
 
-
-# Prefix Verb   URI Pattern                                         Controller#Action
-# login_users
-#        POST   /users/login(.:format)                                    users#login
-#  users
-#        POST   /users(.:format)                                          users#create
-#   user
-#        GET    /users/:id(.:format)                                      users#show
-# workout_weights
-#        GET    /workouts/:workout_id/weights(.:format)                   weights#index
-#        POST   /workouts/:workout_id/weights(.:format)                   weights#create
-# workout_weight
-#        GET    /workouts/:workout_id/weights/:id(.:format)               weights#show
-#        PATCH  /workouts/:workout_id/weights/:id(.:format)               weights#update
-#        PUT    /workouts/:workout_id/weights/:id(.:format)               weights#update
-#        DELETE /workouts/:workout_id/weights/:id(.:format)               weights#destroy
-# workout_cardios
-#        GET    /workouts/:workout_id/cardios(.:format)                   cardios#index
-#        POST   /workouts/:workout_id/cardios(.:format)                   cardios#create
-# workout_cardio
-#        GET    /workouts/:workout_id/cardios/:id(.:format)               cardios#show
-#        PATCH  /workouts/:workout_id/cardios/:id(.:format)               cardios#update
-#        PUT    /workouts/:workout_id/cardios/:id(.:format)               cardios#update
-#        DELETE /workouts/:workout_id/cardios/:id(.:format)               cardios#destroy
-# workouts
-#        GET    /workouts(.:format)                                       workouts#index
-#        POST   /workouts(.:format)                                       workouts#create
-# workout
-#        GET    /workouts/:id(.:format)                                   workouts#show
-#        PATCH  /workouts/:id(.:format)                                   workouts#update
-#        PUT    /workouts/:id(.:format)                                   workouts#update
-#        DELETE /workouts/:id(.:format)                                   workouts#destroy
+#
+# Prefix Verb   URI Pattern                                                Controller#Action
+# user_workout_weights
+# GET    /users/:user_id/workouts/:workout_id/weights(.:format)     weights#index
+# POST   /users/:user_id/workouts/:workout_id/weights(.:format)     weights#create
+# user_workout_weight
+# GET    /users/:user_id/workouts/:workout_id/weights/:id(.:format) weights#show
+# PATCH  /users/:user_id/workouts/:workout_id/weights/:id(.:format) weights#update
+# PUT    /users/:user_id/workouts/:workout_id/weights/:id(.:format) weights#update
+# DELETE /users/:user_id/workouts/:workout_id/weights/:id(.:format) weights#destroy
+# user_workout_cardios
+# GET    /users/:user_id/workouts/:workout_id/cardios(.:format)     cardios#index
+# POST   /users/:user_id/workouts/:workout_id/cardios(.:format)     cardios#create
+# user_workout_cardio
+# GET    /users/:user_id/workouts/:workout_id/cardios/:id(.:format) cardios#show
+# PATCH  /users/:user_id/workouts/:workout_id/cardios/:id(.:format) cardios#update
+# PUT    /users/:user_id/workouts/:workout_id/cardios/:id(.:format) cardios#update
+# DELETE /users/:user_id/workouts/:workout_id/cardios/:id(.:format) cardios#destroy
+#     user_workouts
+# GET    /users/:user_id/workouts(.:format)                         workouts#index
+# POST   /users/:user_id/workouts(.:format)                         workouts#create
+#      user_workout
+# GET    /users/:user_id/workouts/:id(.:format)                     workouts#show
+# PATCH  /users/:user_id/workouts/:id(.:format)                     workouts#update
+# PUT    /users/:user_id/workouts/:id(.:format)                     workouts#update
+# DELETE /users/:user_id/workouts/:id(.:format)                     workouts#destroy
+#       login_users
+# POST   /users/login(.:format)                                     users#login
+#             users
+# POST   /users(.:format)                                           users#create
+#              user
+# GET    /users/:id(.:format)                                       users#show
